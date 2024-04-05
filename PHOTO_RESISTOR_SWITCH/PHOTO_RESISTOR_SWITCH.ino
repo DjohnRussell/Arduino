fce7e95a90819f0575a65123883f708a529d32ff
Daniel John Russell <russelldanieljohn@gmail.com>
2024-04-05 20:41:44 +0200

    ADDAD DESCRIPTION

--- PHOTO_RESISTOR_SWITCH/PHOTO_RESISTOR_SWITCH.ino
+++ PHOTO_RESISTOR_SWITCH/PHOTO_RESISTOR_SWITCH.ino
@@ -14,10 +14,12 @@ void setup() {
   Serial.begin(9600);
 }
 
-//---------------------------------------------------Functions---------------------------------------------------------
-
-
-
+//--------------------------------------------------- the curcut---------------------------------------------------------
+// arduino = ar, breadboard = bb, photoreststor = photo, led green = green, led red = red
+// ar 5V -> bb, ar GND -> bb
+// red nagative -> 220 ohm -> GND and positive -> pin 8
+// green nagative -> 220 ohm -> GND and positive -> pin 7
+// photo one leg to 5V and the other led to A0 and 10K omh -> GND
 //----------------------------------------------------------------------------------------------------------------------
 
 void loop() {
