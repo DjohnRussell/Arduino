5801f0e04fe4ba1e28db9e8995e87ee563f1d99e
Daniel John Russell <russelldanieljohn@gmail.com>
2024-04-15 15:22:41 +0200

    Added comments and removed some unused code

--- FOLLOW_THE_LIGHT_V1/FOLLOW_THE_LIGHT_V1.ino
+++ FOLLOW_THE_LIGHT_V1/FOLLOW_THE_LIGHT_V1.ino
@@ -1,5 +1,14 @@
 #include <Servo.h>
+/*
+----------------------- Description------------------------------
+4 photo resistors
+  - one led to 5V
+  - the other leg to digitalPin (A0, A1, A2 and A3), and 10K ohm resister -> GND
+  - servo wires red to 5V, black to GND and white to pin 9.
+  - infront of the servi conection add a capacitor 100 uf..
 
+
+*/
 const int numPhotoresistors = 4;
 const int servoPin = 9;
 
@@ -17,8 +26,7 @@ const int photoPin_A3 = A3;
 
 Servo myServo;
 
-int photoresistorPins[numPhotoresistors] = {A0, A1, A2, A3};
-int servoStartAngle = 0;
+
 
 void setup() {
    myServo.attach(servoPin); 
@@ -26,17 +34,17 @@ void setup() {
 }
 
 void loop() {
-  //myServo.write(0);
   
-
   photoValueA0 = analogRead(photoPin_A0);
   photoValueA1 = analogRead(photoPin_A1);
   photoValueA2 = analogRead(photoPin_A2);
   photoValueA3 = analogRead(photoPin_A3);
 
+  //Angels based on the value from the sensors (Tested angles to match the sensors placemeant).
+
   // A3
   if(photoValueA0 > photoValueA1 && photoValueA0 > photoValueA2 && photoValueA0 > photoValueA3 ) {
-    myServo.write(140);
+    myServo.write(160);
   }
   // A2
   else if(photoValueA1 > photoValueA0 && photoValueA1 > photoValueA2 && photoValueA1 > photoValueA3 ) {
@@ -48,7 +56,7 @@ void loop() {
   }
   // A0
   else if (photoValueA3 > photoValueA0 && photoValueA3 > photoValueA1 && photoValueA3 > photoValueA2) {
-    myServo.write(19); // Adjust the angle according to your requirement
+    myServo.write(19); 
   }
   
 }
